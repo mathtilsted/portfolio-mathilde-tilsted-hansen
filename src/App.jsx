6d36dc3c92e3b7249584ec235fa61711d1c30b9a
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-06-19 16:34:39 +0200

    anden router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/Homepage";
 import ProjektPage from "./pages/ProjektPage";
 import KontaktPage from "./pages/KontaktPage";
 import OmPage from "./pages/OmPage";
 import NavBar from "./components/NavBar";
+import { HashRouter, Routes, Route } from "react-router";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -15,6 +15,6 @@ export default function App() {
         <Route path="/kontakt-mig" element={<KontaktPage />} />
         <Route path="/om-mig" element={<OmPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
