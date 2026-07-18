d916f8c2c8b70b1ed0c2b714ed17ab6aa79178fa
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-07-18 17:51:56 +0200

    små justeringer på om os og kontakt siderne

--- src/pages/KontaktPage.jsx
+++ src/pages/KontaktPage.jsx
@@ -2,43 +2,45 @@ import "../css/kontakt.css";
 
 export default function KontaktPage() {
   return (
-    <div className="sektion">
-      <p className="titel">Vil du vide mere?</p>
+    <div className="kontakt-siden">
+      <div className="sektion">
+        <p className="titel">Vil du vide mere?</p>
 
-      <div className="kontakt-knapper">
-        <a
-          className="kontakt-knap"
-          href="https://github.com/mathtilsted?tab=repositories"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
+        <div className="kontakt-knapper">
+          <a
+            className="kontakt-knap"
+            href="https://github.com/mathtilsted?tab=repositories"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
 
-        <a
-          className="kontakt-knap"
-          href="https://www.linkedin.com/in/mathilde-tilsted-hansen-125513347/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Linkedin
-        </a>
+          <a
+            className="kontakt-knap"
+            href="https://www.linkedin.com/in/mathilde-tilsted-hansen-125513347/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Linkedin
+          </a>
 
-        <a className="kontakt-knap" href="mailto:mathildetilsted@gmail.com">
-          mathildetilsted@gmail.com
-        </a>
+          <a className="kontakt-knap" href="mailto:mathildetilsted@gmail.com">
+            mathildetilsted@gmail.com
+          </a>
 
-        <a className="kontakt-knap" href="tel:+4551147110">
-          +45 51 14 71 10
-        </a>
+          <a className="kontakt-knap" href="tel:+4551147110">
+            +45 51 14 71 10
+          </a>
 
-        <a
-          className="kontakt-knap"
-          href={`${import.meta.env.BASE_URL}Mathilde-Tilsted-CV.pdf`} //cv download virker både lokalt og efter deploye
-          download
-        >
-          Download CV
-        </a>
+          <a
+            className="kontakt-knap"
+            href={`${import.meta.env.BASE_URL}Mathilde-Tilsted-CV.pdf`} //cv download virker både lokalt og efter deploye
+            download
+          >
+            Download CV
+          </a>
+        </div>
       </div>
     </div>
   );
