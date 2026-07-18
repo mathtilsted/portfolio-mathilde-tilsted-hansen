5463df0564dc4780e34c10631802e8032da8a66b
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-07-18 16:52:26 +0200

    beskrivelse af pubhub konceptet

--- src/components/PubhubDisplay.jsx
+++ src/components/PubhubDisplay.jsx
@@ -14,6 +14,14 @@ export default function Pubhub() {
           Prøv appen her
         </a>
       </div>
+      <div className="midt-container">
+        <p className="display-tekst">
+          Denne case handlede om udviklingen af et app koncept. Formålet var at
+          lave et koncept der kunne skabe værdi for en specifik målgruppe, og
+          som var en app der manglede.
+        </p>
+      </div>
+      <div className="bund-container"></div>
     </>
   );
 }
