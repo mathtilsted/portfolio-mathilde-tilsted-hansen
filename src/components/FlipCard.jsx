0ba64a4b6914f9f5d26c6d6f7ff205235278a3c2
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-07-29 20:51:41 +0200

    flipcard funktion på om mig siden, nyt komponent og ny css fil

---
+++ src/components/FlipCard.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "../css/flipcard.css";
+
+export default function FlipCard({ image, title, text }) {
+  const [flipped, setFlipped] = useState(false);
+
+  return (
+    <div
+      className={`flip-card ${flipped ? "flipped" : ""}`}
+      onClick={() => setFlipped(!flipped)}
+    >
+      <div className="flip-side front">
+        <img src={image} alt={title} />
+      </div>
+
+      <div className="flip-side back">
+        <h3>{title}</h3>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/OmPage.jsx
+++ src/pages/OmPage.jsx
@@ -1,4 +1,7 @@
+import Mig2 from "../assets/om-mig/mig2.jpeg";
+import Mig3 from "../assets/om-mig/mig3.jpeg";
 import Mig4 from "../assets/om-mig/mig4.jpeg";
+import FlipCard from "../components/FlipCard";
 import "../css/om.css";
 
 export default function OmPage() {
@@ -6,25 +9,34 @@ export default function OmPage() {
     <>
       <div className="sektion">
         <div className="titel">
-          <p>Hvem er jeg?</p>
+          <h3>Hvem er jeg?</h3>
         </div>
-        <div className="om-container">
-          <div className="om-tekst">
-            <p>Hej! Jeg hedder Mathilde</p>
-            <p>
-              Jeg er selvfølgelig multimediedesignstuderende. Men jeg laver
-              mange andre ting i min hverdag, som jeg synes er lige så fede som
-              at kode, designe og nørkle med ting på uddannelsen. Jeg er amatør
-              musiker og bruger rigtig meget af min tid på at spille musik og
-              øve op til koncerter med mit band. Det er et brass band. Og jeg
-              elsker det. Jeg bruger også tid på projekter som hækling eller
-              strik. Det giver mig rigtig meget at kunne lidt af mange
-              forskellige ting, samtidig nyder jeg at fordybe mig og jeg går op
-              i at gøre det godt. Jeg vil gerne være tilfreds med det jeg laver
-              og jeg er typen der vil have at tingene bliver gjort ordentligt.
-            </p>
-          </div>
-          <img src={Mig4} alt="et billede af mig, Mathilde" />
+        <p className="om-mig-under-titel">
+          Klik på billederne og lær lidt om mig
+        </p>
+        <div className="billed-container">
+          <FlipCard
+            image={Mig2}
+            title="Musiker"
+            text="I min fritid er jeg amatørmusiker og spiller i Århus Brass Band. 
+            Jeg bruger meget tid på at øve og forberede mig til koncerter og konkurrencer. 
+            Jeg er født ind i en musik familie så jeg ser det som en kæmpe del af den jeg er blevet til."
+          />
+          <FlipCard
+            image={Mig3}
+            title="Ekstrovert"
+            text="Jeg holder meget af at mødes med mine venner og i det hele taget være omkring mennesker.
+            Jeg har selvfølgelig også brug for en pause en gang i mellem, men jeg trives i at holde mig travl
+            og beskræftiget."
+          />
+          <FlipCard
+            image={Mig4}
+            title="Ordentlig"
+            text="Jeg ville elske at skrive at jeg er en håbløs kreativ sjæl der forsvinder ind i boblen når jeg kreerer ting,
+            det er jeg på sin vis også. Jeg har super god koncentration når jeg skal løse opgaver, men mest af alt er jeg ordentlig. 
+            Jeg tager mine ansvar utroligt seriøst og jeg synes det er mega fedt at have overblik og have overskud til, ja at gøre tingene ordentligt.
+            Jeg gør altid mit bedste."
+          />
         </div>
       </div>
     </>
