1138cdd155b6996279c167d98d099b55f2aff9f7
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-07-29 16:27:06 +0200

    er gået igang med om mig siden

--- src/pages/OmPage.jsx
+++ src/pages/OmPage.jsx
@@ -1,3 +1,4 @@
+import Mig4 from "../assets/om-mig/mig4.jpeg";
 import "../css/om.css";
 
 export default function OmPage() {
@@ -7,25 +8,23 @@ export default function OmPage() {
         <div className="titel">
           <p>Hvem er jeg?</p>
         </div>
-        <div className="om-tekst">
-          <p>Hej! Jeg hedder Mathilde</p>
-          <p>
-            Jeg er selvfølgelig multimediedesign studerende. Men jeg laver mange
-            andre ting i min hverdag, som jeg synes er lige så fede som at kode,
-            designe og nørkle med ting på uddannelsen.
-            <br />
-            <br />
-            Jeg er amatør musiker og bruger rigtig meget af min tid på at spille
-            musik og øve op til koncerter med mit band. Det er et brass band. Og
-            jeg elsker det.
-            <br />
-            <br />
-            Jeg bruger også tid på projekter som hækling eller strik. Det giver
-            mig rigtig meget at kunne lidt af mange forskellige ting, samtidig
-            nyder jeg at fordybe mig og jeg går op i at gøre det godt. Jeg vil
-            gerne være tilfreds med det jeg laver og jeg er typen der vil have
-            at tingene bliver gjort ordentligt.
-          </p>
+        <div className="om-container">
+          <div className="om-tekst">
+            <p>Hej! Jeg hedder Mathilde</p>
+            <p>
+              Jeg er selvfølgelig multimediedesignstuderende. Men jeg laver
+              mange andre ting i min hverdag, som jeg synes er lige så fede som
+              at kode, designe og nørkle med ting på uddannelsen. Jeg er amatør
+              musiker og bruger rigtig meget af min tid på at spille musik og
+              øve op til koncerter med mit band. Det er et brass band. Og jeg
+              elsker det. Jeg bruger også tid på projekter som hækling eller
+              strik. Det giver mig rigtig meget at kunne lidt af mange
+              forskellige ting, samtidig nyder jeg at fordybe mig og jeg går op
+              i at gøre det godt. Jeg vil gerne være tilfreds med det jeg laver
+              og jeg er typen der vil have at tingene bliver gjort ordentligt.
+            </p>
+          </div>
+          <img src={Mig4} alt="et billede af mig, Mathilde" />
         </div>
       </div>
     </>
