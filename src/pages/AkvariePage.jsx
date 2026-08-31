9b1fc6a58a17ddd5359c3271177d025712eb1d44
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-08-31 15:47:52 +0200

    jeg har tilføjet en masse til akvarie siden og rodet med noget css

--- src/pages/AkvariePage.jsx
+++ src/pages/AkvariePage.jsx
@@ -1,31 +1,41 @@
 import ProjektLayout from "../components/ProjektLayout";
 import AkvarieProto from "../assets/akvarie/akvarie-prototype.png";
 import BrugerTest from "../assets/akvarie/brugertest.png";
+import Visual1 from "../assets/akvarie/visual1.png";
+import Visual2 from "../assets/akvarie/visual2.png";
+import Visual3 from "../assets/akvarie/visual3.png";
+import Visual4 from "../assets/akvarie/visual4.png";
 
 export default function Akvarie() {
   return (
     <ProjektLayout
       label="Storcenter Nord · Eksamensprojekt 2025"
-      title="Akvarie"
-      intro="Et interaktivt læringsspil designet til børn i alderen 2–3 år, med fokus på leg, læring og brugeroplevelse. Startskuddet som multimediedesigner."
-      introLeft="I denne case arbejdede jeg med spildesign til børn, der leger ved akvariet i bunden af Storcenter Nord. Målet var at skabe et legende univers, der kunne fange børnenes opmærksomhed samtidig med, at spillet formidlede viden om havet."
-      introRight="Projektet er en videreudvikling og optimering af et tidligere semesterprojekt. Som en del af mit eksamensprojekt valgte jeg at redesigne spillet fra bunden med udgangspunkt i de erfaringer og idéer, jeg havde samlet op under det oprindelige projekt."
-      buttonText="Prøv spillet her"
+      title="Akvariet"
+      intro="I dette projekt blev vi bedt om at skabe en interaktiv oplevelse til børn. Jeg skabte et spil til børn i alderen 2-3 år, hvor jeg fokuserede på leg, 
+      enkelthed og hygge. Gennem projektet lærte jeg helt vildt meget om design til børn, 
+      at forholde sig til en bestemt målgruppe og at justere når man har testet sin prototype."
+      buttonText="Se live projekt"
       buttonLink="https://eaa25mtha.github.io/eksamens-projekt-mathilde/"
       heroImage={AkvarieProto}
       heroImageAlt="Designkomponenter fra akvariespillet"
-      prototypes={
-        [
-          /*{
-          src: AkvarieProto1,
+      prototypes={[
+        {
+          src: Visual1,
           alt: "Første prototype af akvariespillet",
         },
         {
-          src: AkvarieProto2,
-          alt: "Anden prototype af akvariespillet",
-        },*/
-        ]
-      }
+          src: Visual2,
+          alt: "Første prototype af akvariespillet",
+        },
+        {
+          src: Visual3,
+          alt: "Første prototype af akvariespillet",
+        },
+        {
+          src: Visual4,
+          alt: "Brugertest af akvariespillet",
+        },
+      ]}
       reflectionTitle="Design til de mindste"
       reflectionText="Jeg tog udgangspunkt i børn i aldersgruppen 2–3 år. 
       Det er en meget ung, men også spændende og livlig målgruppe at designe til. Under processen arbejdede jeg blandt andet med designprincipper 
