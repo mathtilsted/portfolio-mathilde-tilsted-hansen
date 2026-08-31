e23ec2a4d0f05ec40bc6aaa046e3617e462e79ec
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-08-31 17:31:16 +0200

    jeg er gået videre til pubhub siden

--- src/pages/ProjektPage.jsx
+++ src/pages/ProjektPage.jsx
@@ -3,18 +3,17 @@ import "../css/projekt-page.css";
 import PubhubLogo from "../assets/pubhub-ikon.svg";
 import AkvarieLogo from "../assets/storcenternord-ikon.svg";
 import BlandetLogo from "../assets/blandet-ikon.svg";
+import Baggrund4 from "../assets/baggrunde/baggrund4.png";
 
 export default function ProjektPage() {
   return (
     <>
-      <h1 className="projekt-page-titel">
-        Vælg et projekt - se lidt af processen
-      </h1>
+      <img className="baggrund-projekt-siden" src={Baggrund4} alt="" />
       <main className="projekter-grid">
         <Link className="projekt-card" to="/pubhub">
           <img src={PubhubLogo} alt="PubHub projekt" />
 
-          <div className="projekt-card-overlay">
+          <div className="projekt-card-text">
             <h3>PubHub</h3>
             <p>App-koncept · 2026 · React · Figma</p>
           </div>
@@ -23,7 +22,7 @@ export default function ProjektPage() {
         <Link className="projekt-card" to="/akvarie">
           <img src={AkvarieLogo} alt="Akvarie projekt" />
 
-          <div className="projekt-card-overlay">
+          <div className="projekt-card-text">
             <h3>Akvarie-spillet</h3>
             <p>Interaktiv løsning · 2025 · JavaScript · Figma</p>
           </div>
@@ -32,7 +31,7 @@ export default function ProjektPage() {
         <Link className="projekt-card" to="/blandede-projekter">
           <img src={BlandetLogo} alt="Blandede projekter" />
 
-          <div className="projekt-card-overlay">
+          <div className="projekt-card-text">
             <h3>Blandede Projekter</h3>
             <p>Legeplads · Eksperimenter · Udfordring</p>
           </div>
--- src/pages/PubHubPage.jsx
+++ src/pages/PubHubPage.jsx
@@ -1,19 +1,22 @@
 import ProjektLayout from "../components/ProjektLayout";
+import PubPlakat from "../assets/blandet/pubhub-plakat.png";
+import Moodboard from "../assets/pubhub/moodboard.png";
 
 export default function PubHubPage() {
   return (
     <ProjektLayout
-      label="Webdesign · UX/UI"
+      label="React · Webapp"
       title="PubHub"
-      intro="En digital oplevelse skabt med fokus på..."
+      intro="En digital WebApp designet til unge studerende i Aarhus med det formål at give dem hele Aarhus natteliv i én app. Nemt, sjovt, underholdende og drunk-safe."
       buttonText="Se live projekt"
       buttonLink="#"
-      heroImage={null}
+      heroImage={PubPlakat}
       heroImageAlt="PubHub website"
       Prototypes={[]}
       reflectionTitle="Hvad lærte jeg?"
-      reflectionText="Her kan jeg beskrive mine vigtigste erfaringer fra projektet."
-      reflectionImage={null}
+      reflectionText="I PubHub projektet lærte jeg rigtig meget om at have god teknik bag det design vi laver. Det at sørge for at have et ordentligt designsystem der er til at finde rundt i for at have et godt og nemt samarbejde.
+      Jeg lærte også en masse craft, fordi jeg sad og lavede rigtig mange af elementerne selv, da vi kun var 2 i gruppen, så det gav en masse ansvar og follow-through fordi tingene skulle fikses."
+      reflectionImage={Moodboard}
       reflectionImageAlt="PubHub refleksion"
     />
   );
