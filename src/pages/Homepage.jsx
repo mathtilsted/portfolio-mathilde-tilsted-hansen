a45fee254ab7234f45ed94e119a5059846df7679
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-09-18 08:30:12 +0200

    tweaks

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,10 @@
-import LottieModule from "lottie-react";
-import KompetenceAnimation from "../animationer/kompetencer-anim.json";
+//import LottieModule from "lottie-react";
+//import KompetenceAnimation from "../animationer/kompetencer-anim.json";
 import Baggrund3 from "../assets/baggrunde/baggrund3.png";
 import Profil from "../assets/profil.jpeg";
 
 export default function HomePage() {
-  const Lottie = LottieModule.default;
+  //const Lottie = LottieModule.default;
 
   return (
     <main>
@@ -21,18 +21,19 @@ export default function HomePage() {
           <section className="homepage-tekst">
             <h1 className="homepage-titel">Mathilde Tilsted Hansen.</h1>
             <p>
-              Hej! Det er mig der er Mathilde. Jeg søger praktik til Januar
-              2027. Jeg er multimediedesignstuderende på 3. semester på
+              Hej! Det er mig der er Mathilde. Jeg skal i praktik i 10 uger til
+              januar 2027. Jeg er multimediedesignstuderende på 3. semester på
               Erhvervsakademi Aarhus og brænder for at skabe løsninger der både
               er fede OG funktionelle. Jeg læser på IXD linjen på akademiet og
-              har mine interesser i UX/UI samt React og Figma.
+              har mine interesser i UX/UI samt React og Figma. Jeg vil gerne
+              lære så meget jeg kan, så lad os få en snak om en praktikplads!
             </p>
           </section>
         </div>
       </section>
-      <div className="kompetencer">
+      {/* <div className="kompetencer">
         <Lottie animationData={KompetenceAnimation} loop={true} />
-      </div>
+      </div>*/}
     </main>
   );
 }
