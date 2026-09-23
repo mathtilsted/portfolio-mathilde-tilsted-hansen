dc6cb255df4bc6fdf8b0ffc4335e472e075526a5
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-09-23 09:20:30 +0200

    nunito som brødtekst font og en ændring i teksten på homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -22,9 +22,10 @@ export default function HomePage() {
               Hej! Det er mig der er Mathilde. Jeg skal i praktik i 10 uger til
               januar 2027. Jeg er multimediedesignstuderende på 3. semester på
               Erhvervsakademi Aarhus og brænder for at skabe løsninger der både
-              er fede OG funktionelle. Jeg læser på IXD linjen på akademiet og
-              har mine interesser i UX/UI samt React og Figma. Jeg vil gerne
-              lære så meget jeg kan, så lad os få en snak om en praktikplads!
+              er fede OG funktionelle. Jeg læser på interaction design linjen på
+              akademiet og har mine styrker i UX/UI samt React og Figma. Jeg vil
+              gerne lære så meget jeg kan, så lad os få en snak om en
+              praktikplads!
             </p>
           </div>
         </div>
